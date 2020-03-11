2527a201b6d3aa7746d7b68e26bca24936fd7f12
Rafael Salvati <rafaels@infoteam.local>
2020-03-11 10:15:02 +0100

    Changin the menu for LoRa Gateway: from Network menu to the LoRaWAN menu

--- luci-app-pkg-fwd/files/lora_controller.lua
+++ luci-app-pkg-fwd/files/lora_controller.lua
@@ -2,5 +2,5 @@
 module("luci.controller.lora.lora",package.seeall)
 
 function index()
-    entry({"admin","network","lora"}, cbi("lora/lora_gateway"),_("LoRa Gateway"),99).index=true
+    entry({"admin","lorawan","lora"}, cbi("lora/lora_gateway"),_("LoRa Gateway"),99).index=true
 end
