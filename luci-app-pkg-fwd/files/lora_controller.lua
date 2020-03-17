8cd67ecebc2b206c0bb9d54f6f3b1aa664bad403
Titouan Mesot <titouan.mesot@infoteam.ch>
2020-03-17 14:36:43 +0100

    luci-app-pkg-fwd: clarify cbi naming

--- luci-app-pkg-fwd/files/lora_controller.lua
+++ luci-app-pkg-fwd/files/lora_controller.lua
@@ -1,7 +1,7 @@
 
-module("luci.controller.lora.lora",package.seeall)
+module("luci.controller.lora.lora-pkt-fwd",package.seeall)
 
 function index()
     entry({"admin", "lorawan"}, firstchild(), _("LoRaWAN"), 50).index = true
-    entry({"admin","lorawan","lora"}, cbi("lora/lora_gateway"),_("LoRa Gateway"),99).index=true
+    entry({"admin","lorawan","lora-pkt-fwd"}, cbi("lora/lora_gateway"),_("LoRa Gateway"),99).index=true
 end
